42f1ce16515ab9daee5e4e65f2175252390ed08b
powerdragonfire <mihirgandecha@outlook.com>
2025-10-02 00:42:50 +0100

    Reverted package upgrades: TODO fix test playright

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,13 +2,7 @@ import { defineConfig } from "vitest/config";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [
-        react({
-            babel: {
-                plugins: ["babel-plugin-react-compiler"],
-            },
-        }),
-    ],
+    plugins: [react()],
 
     test: {
         globals: true, // so can use `describe`/`it` without importing them
